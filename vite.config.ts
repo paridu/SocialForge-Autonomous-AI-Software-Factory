3b7f0342483062304f8b8f44ddf254c1e6fb9cb5
Parid Upapong <paris.u2528@gmail.com>
2026-01-07 10:50:14 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./")
+    }
+  },
   build: {
     outDir: "dist",
     assetsDir: "assets",
